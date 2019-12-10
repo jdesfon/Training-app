3b2f2f6f66f8669ae1c9d4293510f94a7eebfd4f
jdesfon <jdesfon@gmail.com>
2019-12-10 08:12:25 +0100

    feat: Exercise view

---
+++ src/views/Exercise.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>exercise view</h1>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { EXERCISE } from "@/store-types/module-names";
+import { ADD_SET } from "@/store-types/mutations-types";
+export default {
+  name: "Exercise",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  mounted() {
+    this.title = this.$route.params.name;
+  },
+  methods: {
+    ...mapMutations(EXERCISE, {
+      addSet: ADD_SET
+    })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.Exercise {
+}
+</style>
