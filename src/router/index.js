61271c6523a69a49c741d7e4e59ac15af6fa330b
jdesfon <jdesfon@gmail.com>
2019-12-10 08:12:08 +0100

    feat: exercise route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Exercise from "../views/Exercise";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/exercise/:name",
+    name: "Exercise",
+    component: Exercise
   }
 ];
 
