4c50816d9f2f99e13cd9a6a1edf6450e8d9b7882
jdesfon <jdesfon@gmail.com>
2019-12-11 17:22:56 +0100

    feat(main): apexchart component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueApexCharts from "vue-apexcharts";
+
+Vue.component("apexchart", VueApexCharts);
 
 import "@/assets/style/index.scss";
 import vuetify from "./plugins/vuetify";
