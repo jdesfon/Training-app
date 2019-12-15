9f02d9a4809ec4bd3aa5327ddac24e7cb79dd22e
jdesfon <jdesfon@gmail.com>
2019-12-15 16:52:21 +0100

    feat(login view): login view

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div id="app">
     <router-view />
   </div>
-  <!--<v-content>
-    </v-content>
-  </v-app>-->
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
+import Lodash from "lodash"
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Exercise from "../views/Exercise";
+import Login from "../views/auth/index.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
   {
     path: "/",
     name: "Home",
@@ -22,6 +29,52 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+function nextFactory(context, middleware, index) {
+  const subsequentMiddleware = middleware[index]
+  if (!subsequentMiddleware) {
+    return context.next
+  }
+
+  return (...parameters) => {
+    context.next(...parameters)
+    const nextMiddleware = nextFactory(context, middleware, index + 1)
+    subsequentMiddleware({ ...context, next: nextMiddleware })
+  }
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.name === router.currentRoute.name) {
+    return next()
+  }
+
+  // We extract the middleware inside meta middleware from the parent to the current
+  const middleware = to.matched.reduce((acc, val) => {
+    if (!Lodash.isEmpty(val.meta)) {
+      if (Array.isArray(val.meta.middleware)) {
+        acc.push(...val.meta.middleware)
+      } else {
+        acc.push(val.meta.middleware)
+      }
+    }
+    return acc
+  }, [])
+
+  if (!Lodash.isEmpty(middleware)) {
+    const context = {
+      from,
+      next,
+      router,
+      to,
+      store,
+    }
+    const nextMiddleware = nextFactory(context, middleware, 1)
+
+    return middleware[0]({ ...context, next: nextMiddleware })
+  }
+
+  return next()
 });
 
 export default router;
