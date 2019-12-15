5fc038299ae75a3508f9bae516b8b4b82d7904f1
jdesfon <jdesfon@gmail.com>
2019-12-15 15:30:02 +0100

    feat(auth): user store auth
    AWS amplify use authentication

--- src/store-types/mutations-types.js
+++ src/store-types/mutations-types.js
@@ -1,1 +1,4 @@
+export const SET_USER = "SET_USER";
+export const REMOVE_USER = "REMOVE_USER";
+
 export const ADD_SET = "ADD_SET";
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,1 +1,59 @@
-export const namespaces = true;
+import { Auth } from 'aws-amplify'
+
+import {
+    SIGN_IN,
+    SIGN_UP,
+    SIGN_OUT
+} from '@/store-types/actions-types'
+
+import {
+    SET_USER,
+    REMOVE_USER
+} from '@/store-types/mutations-types'
+
+export const actions = {
+    async [SIGN_IN]({commit}, { email, password }) {
+        try {
+            const user = await Auth.signIn(email, password)
+            commit(SET_USER, user)
+            alert('logged in')
+        } catch (error) {
+            alert(error.message)
+        }
+    },
+    async [SIGN_UP]({commit}, { email, password }) {
+        try {
+            const user = await Auth.signUp(email, password)
+            commit(SET_USER, user)
+            alert('signed in')
+        } catch (error) {
+            alert(error.message)
+        }
+    },
+    async [SIGN_OUT]({ commit }) {
+        try {
+            await Auth.signOut()
+            alert('logged out')
+        } catch (error) {
+            alert(error.message)
+        }
+    },
+}
+
+export const mutations = {
+    [SET_USER](state, user) {
+        state.user = user
+        state.isAuthenticated = true
+    },
+    [REMOVE_USER](state) {
+        state.user = null
+        state.isAuthenticated = false
+    }
+}
+
+export const state = () => ({
+    user: null,
+    isAuthenticated: false,
+})
+
+export const namespaced = true
