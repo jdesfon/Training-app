533d133fe8dedabcff9d6d611506fc5f3b565c23
jdesfon <jdesfon@gmail.com>
2019-12-15 15:30:59 +0100

    feat(auth): aws amplify setup and configuration

--- src/main.js
+++ src/main.js
@@ -1,13 +1,38 @@
 import Vue from "vue";
+import Amplify from 'aws-amplify';
+import VueApexCharts from "vue-apexcharts";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueApexCharts from "vue-apexcharts";
+import config from "./config";
+import vuetify from "./plugins/vuetify";
+import "@/assets/style/index.scss";
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "exercises",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+      {
+        name: "sets",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+})
 
 Vue.component("apexchart", VueApexCharts);
-
-import "@/assets/style/index.scss";
-import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
