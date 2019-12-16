3868a4277f3942c52f4dd248ba37d47c81cd120f
jdesfon <jdesfon@gmail.com>
2019-12-16 13:26:46 +0100

    feat(listexercises): add listExercises api route in store

--- src/store-types/actions-types.js
+++ src/store-types/actions-types.js
@@ -2,3 +2,5 @@ export const SIGN_IN = 'SIGN_IN';
 export const SIGN_UP = 'SIGN_UP';
 export const SIGN_OUT = 'SIGN_OUT';
 export const IS_CURRENT_SESSION = "IS_CURRENT_SESSION";
+
+export const FETCH_EXERCISES = "FETCH_EXERCISES";
--- src/store/modules/exercise.js
+++ src/store/modules/exercise.js
@@ -1,58 +1,42 @@
-import { ADD_SET } from "@/store-types/mutations-types";
-import { GET_EXERCISES, GET_SETS } from "@/store-types/getters-types";
+import { API } from 'aws-amplify'
+import endpoints from '../../constants/endpoints'
+import config from '../../config'
+import { FETCH_EXERCISES } from '@/store-types/actions-types'
+import { ADD_SET, SET_EXERCISES } from '@/store-types/mutations-types'
+import { GET_EXERCISES, GET_SETS } from '@/store-types/getters-types'
+
+
+export const actions = {
+    [FETCH_EXERCISES]({ commit }) {
+        return API.get(config.API_NAME, endpoints.listExercises)
+            .then(res => {
+              commit(SET_EXERCISES, res.Items)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    },
+}
 
 export const mutations = {
-  [ADD_SET]: (state, set) => {
-    set.createdAt = new Date();
-    state.sets.push(set);
-  }
-};
+    [ADD_SET]: (state, set) => {
+        set.createdAt = new Date()
+        state.sets.push(set)
+    },
+    [SET_EXERCISES]: (state, exercises) => {
+        state.exercises = exercises
+    },
+}
 
 export const getters = {
-  [GET_EXERCISES]: state => state.exercises,
-  [GET_SETS]: state => exerciseSlug =>
-    state.sets.filter(set => set.exercise.slug === exerciseSlug)
-};
+    [GET_EXERCISES]: state => state.exercises,
+    [GET_SETS]: state => exerciseSlug =>
+        state.sets.filter(set => set.exercise.slug === exerciseSlug),
+}
 
 export const state = {
-  exercises: [
-    {
-      uid: "1",
-      name: "push ups",
-      slug: "push-ups"
-    },
-    {
-      uid: "2",
-      name: "pull ups",
-      slug: "pull-ups"
-    },
-    {
-      uid: "3",
-      name: "squats",
-      slug: "squats"
-    },
-    {
-      uid: "4",
-      name: "dips",
-      slug: "dips"
-    },
-    {
-      uid: "5",
-      name: "lunges",
-      slug: "lunges"
-    },
-    {
-      uid: "6",
-      name: "burpees",
-      slug: "burpees"
-    },
-    {
-      uid: "7",
-      name: "muscle ups",
-      slug: "muscle-ups"
-    }
-  ],
-  sets: []
-};
+    exercises: [],
+    sets: [],
+}
 
-export const namespaced = true;
+export const namespaced = true
