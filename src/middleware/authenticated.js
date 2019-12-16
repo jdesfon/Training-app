8c7bccec07606ae980c3aa2f01bedeccc084a882
jdesfon <jdesfon@gmail.com>
2019-12-16 13:32:32 +0100

    refactor: remove console.log

--- src/middleware/authenticated.js
+++ src/middleware/authenticated.js
@@ -1,13 +1,9 @@
-export default function ({ store, router, next, to }) {
-    const { isAuthenticated } = store.state.user
-    console.log('middleware', {
-        name: to.name,
-        isAuthenticated
-    })
-    if (isAuthenticated && to.name === 'login') {
-        return router.replace({ name: 'home' })
-    } else if (!isAuthenticated && to.name !== 'login') {
-        return router.replace({ name: 'login' })
-    }
-    return next()
+export default function({ store, router, next, to }) {
+  const { isAuthenticated } = store.state.user;
+  if (isAuthenticated && to.name === "login") {
+    return router.replace({ name: "home" });
+  } else if (!isAuthenticated && to.name !== "login") {
+    return router.replace({ name: "login" });
+  }
+  return next();
 }
