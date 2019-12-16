fe7214385af288456d6f2c3ee59b9b121e33e3d5
jdesfon <jdesfon@gmail.com>
2019-12-16 13:25:49 +0100

    refactor(constants): add constants for routes endpoints

---
+++ src/constants/endpoints.js
@@ -0,0 +1,9 @@
+export default {
+    createExercise: '/exercises',
+    getExercise: id => `/exercises/${id}`,
+    listExercises: '/exercises',
+    creatSet: '/sets',
+    listSets: '/sets',
+    updateSet: id => `/sets/${id}`,
+    deleteSet: id => `/sets/${id}`
+}
