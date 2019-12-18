35ac7134a3978c83370b96eb478fd6f5d13fd171
jdesfon <jdesfon@gmail.com>
2019-12-18 11:30:27 +0100

    docs(setshelper): functions documentation

--- src/helpers/setsHelper.js
+++ src/helpers/setsHelper.js
@@ -1,5 +1,27 @@
 import moment from "moment";
 
+/**
+ * @typedef set
+ * @type {object}
+ * @property {string} setId - unique set id
+ * @property {string} userId - user id
+ * @property {string} exerciseId - exercise id
+ * @property {number} reps - number of rep
+ * @property {number} dayOfYear - day number in year
+ * @property {number} weekInYear - week number in year
+ * @property {number} monthInYear - month number in year
+ * @property {number} year - year
+ * @property {string} createdAt - creation date
+ */
+
+
+/**
+ * Calculate total of reps from sets array
+ *
+ * @export
+ * @param {set[]} sets
+ * @returns {number}
+ */
 export function getTotalReps(sets) {
   return sets.reduce((acc, curr) => (acc += curr.reps), 0);
 }
