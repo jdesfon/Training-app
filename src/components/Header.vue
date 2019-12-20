9213014d6be456a5f12b2d8029dbac0a704cfc29
jdesfon <jdesfon@gmail.com>
2019-12-20 15:40:56 +0100

    feat(header): add back navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
     <div class="header">
+        <v-btn v-if="nav" fab small depressed :to="to">
+            <v-icon>keyboard_arrow_left</v-icon>
+        </v-btn>
         <h1>{{ title }}</h1>
     </div>
 </template>
@@ -12,6 +15,14 @@ export default {
             type: String,
             required: true,
         },
+        to: {
+            type: String,
+            default: () => '/',
+        },
+        nav: {
+            type: Boolean,
+            default: () => false,
+        },
     },
 }
 </script>
@@ -19,6 +30,8 @@ export default {
 <style lang="scss" scoped>
 .header {
     width: 100%;
+    display: flex;
+    align-items: center;
     color: #424242;
     border-bottom: 1px solid black;
     text-align: left;
