44f0816570997dbcb5281c1ed26aa8436064afa4
jdesfon <jdesfon@gmail.com>
2019-12-20 16:18:04 +0100

    feat(notifications): add notifications system

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
     <div id="app">
         <router-view />
+        <v-snackbar v-model="snackbar" bottom vertical :color="color" :timeout="timeout">
+            {{ text }}
+        </v-snackbar>
     </div>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex'
+import { NOTIFICATION } from './store-types/module-names'
+import { UPDATE_SNACKBAR } from './store-types/mutations-types'
+import {
+    GET_SNACKBAR,
+    GET_SNACKBAR_MESSAGE,
+    GET_SNACKBAR_TYPE,
+    GET_SNACKBAR_TIMEOUT,
+} from './store-types/getters-types'
+
 export default {
     name: 'App',
+    methods: {
+        ...mapMutations(NOTIFICATION, {
+            updateSnackbar: UPDATE_SNACKBAR,
+        }),
+    },
+    computed: {
+        ...mapGetters(NOTIFICATION, {
+            snackbar: GET_SNACKBAR,
+            text: GET_SNACKBAR_MESSAGE,
+            color: GET_SNACKBAR_TYPE,
+            timeout: GET_SNACKBAR_TIMEOUT,
+        }),
+    },
 }
 </script>
 
