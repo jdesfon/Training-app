3b74b80f735b8dcf7a9fd8c8469b09a7ec6880c8
jdesfon <jdesfon@gmail.com>
2019-12-22 21:37:52 +0100

    feat(signout): add signout view

--- src/views/auth/SignOut.vue
+++ src/views/auth/SignOut.vue
@@ -1,10 +1,27 @@
 <template>
-    <div>logout</div>
+    <div class="signOut">
+        <v-btn to="/" absolute top left icon large>
+            <v-icon large>keyboard_arrow_left</v-icon>
+        </v-btn>
+        <v-btn @click="handleSubmit">sign out</v-btn>
+    </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import { USER } from '../../store-types/module-names'
+import { SIGN_OUT } from '../../store-types/actions-types'
+
 export default {
-    name: 'Logout',
+    name: 'SignOut',
+    methods: {
+        ...mapActions(USER, {
+            signOut: SIGN_OUT,
+        }),
+        handleSubmit() {
+            this.signOut()
+        },
+    },
 }
 </script>
 
