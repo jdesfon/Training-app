e323bd2a133dc7ed4ccf8c2c5b6a41a112266016
jdesfon <jdesfon@gmail.com>
2019-12-23 17:01:46 +0100

    improvement(exercise): create container

--- src/views/Exercise.vue
+++ src/views/Exercise.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="exercise">
-        <PageLoader v-if="isLoading" />
-        <Header :title="exerciseSlug" nav :to="'/exercises'" />
-        <div class="singleNumber-container">
-            <template v-for="(stat, index) of numberStats">
-                <template v-if="index === 3">
-                    <SingleNumber
-                        class="singleNumber"
-                        :key="index"
-                        :title="stat.name"
-                        :value="stat.value"
-                        :background-color="'#ff1744'"
-                        :title-color="'#ffffff'"
-                        :number-color="'#ffffff'"
-                    />
+        <div class="exercise__container">
+            <PageLoader v-if="isLoading" />
+            <Header :title="exerciseSlug" nav :to="'/exercises'" />
+            <div class="singleNumber-container">
+                <template v-for="(stat, index) of numberStats">
+                    <template v-if="index === 3">
+                        <SingleNumber
+                            class="singleNumber"
+                            :key="index"
+                            :title="stat.name"
+                            :value="stat.value"
+                            :background-color="'#ff1744'"
+                            :title-color="'#ffffff'"
+                            :number-color="'#ffffff'"
+                        />
+                    </template>
+                    <template v-else>
+                        <SingleNumber class="singleNumber" :key="index" :title="stat.name" :value="stat.value" />
+                    </template>
                 </template>
-                <template v-else>
-                    <SingleNumber class="singleNumber" :key="index" :title="stat.name" :value="stat.value" />
-                </template>
-            </template>
-        </div>
+            </div>
 
-        <BaseHeatMap class="baseHeatMap" :width="heatMap.width" :height="heatMap.height" :series="heatMap.series" />
-        <BaseBarChart
-            class="baseBarChart"
-            :width="barChart.width"
-            :height="barChart.height"
-            :series="barChart.series"
-        />
+            <BaseHeatMap class="baseHeatMap" :width="heatMap.width" :height="heatMap.height" :series="heatMap.series" />
+            <BaseBarChart
+                class="baseBarChart"
+                :width="barChart.width"
+                :height="barChart.height"
+                :series="barChart.series"
+            />
+        </div>
     </div>
 </template>
 
@@ -111,30 +113,40 @@ export default {
 
 <style lang="scss" scoped>
 .exercise {
-    background-color: #ffffff;
-    min-height: 100vh;
+    background-image: url('../assets/svg/wave.svg');
+    background-size: contain;
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 342px;
-    margin: 0 auto;
+    justify-content: center;
 
-    .singleNumber-container {
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-
-        .singleNumber {
-            width: calc(50% - 0.25rem);
-            margin: 0.25rem 0;
-        }
+    @media (max-width: 960px) {
+        background-size: cover;
     }
 
-    .baseHeatMap,
-    .baseBarChart {
-        width: 100%;
-        margin: 0.25rem 0;
+    &__container {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 342px;
+        margin: 0 auto;
+
+        .singleNumber-container {
+            width: 100%;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+
+            .singleNumber {
+                width: calc(50% - 0.25rem);
+                margin: 0.25rem 0;
+            }
+        }
+
+        .baseHeatMap,
+        .baseBarChart {
+            width: 100%;
+            margin: 0.25rem 0;
+        }
     }
 }
 </style>
